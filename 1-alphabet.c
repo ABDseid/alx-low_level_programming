047cfc126ecd7c45362e9a3695c8cdabd8bde139
ABDseid <abddseid@gmail.com>
2022-12-13 00:07:37 -0800

    task 1

---
+++ 1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - entry point
+ *
+ * Return: 0
+ * */
+
+void print_alphabet(void)
+
+{
+	char c = "a";
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}
